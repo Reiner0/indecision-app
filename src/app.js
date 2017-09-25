9aa1ed27f22d006416b3de8c920babbe1480c58f
Reiner Leal <reinerleal@gmail.com>
2017-09-24 21:47:18 -0700

    notes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ console.log("App.js is running!");
 
 // var is pre ES6
 
+/*
+babel src/playground/es6-let-const.js 
+  --out-file=public/scripts/app.js --presets=env,react --watch
+*/
+
 let app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
